debe5c458d78ccb469a929f4b61a0393768b5435
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-02-21 10:31:08 +0800

    edit graphql path

--- app/start.py
+++ app/start.py
@@ -8,7 +8,4 @@ from app.mutator import Mutation
 app = FastAPI()
 
 
-
-
-
-app.add_route('/auth/graphql', GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query, mutation=Mutation)))
