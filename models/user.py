aebd45454b0434999d21d6ad29e7363f92467e57
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-05-11 01:18:34 +0800

    remove referance model

--- models/user.py
+++ models/user.py
@@ -1,11 +1,5 @@
 from configs.database import Model
-from orator.orm import has_one, has_many
-from .account import Account
-
-
 class User(Model):
 
-    @has_one
-    def account(self):
-        return Account
+    pass
 
