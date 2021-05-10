f7c7bf496ad0b4e9b7b5be762b285b08eed6f19a
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-05-11 03:10:42 +0800

    missing @

--- app/schema.py
+++ app/schema.py
@@ -1,11 +1,5 @@
 import graphene
 
-def checkUser(*args, next, **kwargs):
-    print(args)
-    print(kwargs)
-    return next(*args, **kwargs)
-
-
 class Query(graphene.ObjectType):
     say_hello = graphene.String(name=graphene.String(default_value='Test Driven'))
 
