4c580599c89e28fa808fc7ba3a2a9a7ce4f8174e
Muhammad Abdurrahman <rachman.sd@gmail.com>
2021-05-18 00:19:08 +0800

    remove file not require by default

--- vendor/__init__.py
+++ vendor/__init__.py
@@ -1,4 +1,2 @@
-
 from app import start
-import vendor.auth
 app=start.app
--- vendor/auth.py
+++
@@ -1,4 +0,0 @@
-from configs.application import configs
-from fastql_packages.authentication import Auth
-
-authentication = Auth(configs)
